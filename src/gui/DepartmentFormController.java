c4ce3a91a3154266f4935319c295f12f5c803902
joaovitorsb97 <jvs.bastos@gmail.com>
2022-07-15 14:43:30 -0300

    Validation exception

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,7 +3,9 @@ package gui;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import db.DbException;
 import gui.listeners.DataChangeListener;
@@ -18,16 +20,17 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exceptions.ValidationException;
 import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
 
 	private Department department;
-	
+
 	private DepartmentService service;
-	
+
 	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
-	
+
 	@FXML
 	private TextField txtId;
 	@FXML
@@ -41,7 +44,7 @@ public class DepartmentFormController implements Initializable {
 
 	@FXML
 	public void onBtnSaveAction(ActionEvent event) {
-		if(department == null || service == null) {
+		if (department == null || service == null) {
 			throw new IllegalStateException("Department or Service was null!");
 		}
 		try {
@@ -50,23 +53,37 @@ public class DepartmentFormController implements Initializable {
 			Alerts.showAlert("Success!", null, "Department inserted with success!", AlertType.CONFIRMATION);
 			notifyDataChangeListener();
 			Utils.currentStage(event).close();
-		}
-		catch(DbException e) {
+		} catch (DbException e) {
 			Alerts.showAlert("Error to save department data", null, e.getMessage(), AlertType.ERROR);
+		}catch(ValidationException e) {
+			setLabelError(e.getErrors());
 		}
 	}
 
 	private void notifyDataChangeListener() {
-		for(DataChangeListener listeners : dataChangeListeners) {
+		for (DataChangeListener listeners : dataChangeListeners) {
 			listeners.onDataChanged();
 		}
-		
+
 	}
 
 	private Department getFormData() {
 		Department department = new Department();
+
+		ValidationException exception = new ValidationException("Validation Error");
+
 		department.setId(Utils.tryParseToInt(txtId.getText()));
+
+		if (txtName.getText() == null || txtName.getText().trim().equals("")) {
+			exception.addErrors("name", "Field name can't be empty");
+		}
+		
 		department.setName(txtName.getText());
+		
+		if (exception.getErrors().size() > 0) {
+			throw exception;
+		}
+		
 		return department;
 	}
 
@@ -78,27 +95,35 @@ public class DepartmentFormController implements Initializable {
 	public void initialize(URL url, ResourceBundle rb) {
 		initializeNodes();
 	}
-	
+
 	public void initializeNodes() {
 		Constraints.setTextFieldInteger(txtId);
 		Constraints.setTextFieldMaxLength(txtName, 40);
 	}
-	
+
 	public void setDepartment(Department department) {
 		this.department = department;
 	}
-	
+
 	public void setDepartmentService(DepartmentService service) {
 		this.service = service;
 	}
-	
+
 	public void subscribeDataChangeListener(DataChangeListener list) {
 		dataChangeListeners.add(list);
 	}
-	
+
 	public void updateFormData() {
 		txtId.setText(String.valueOf(department.getId()));
 		txtName.setText(department.getName());
+	}
+	
+	public void setLabelError(Map<String, String> errors) {
+		Set<String> errorLabel = errors.keySet();
+		
+		if(errorLabel.contains("name")) {
+			labelError.setText(errors.get("name"));
+		}
 	}
 
 }
