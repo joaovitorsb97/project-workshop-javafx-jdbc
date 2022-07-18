9c7d3c72608b55685b7346fbf382635b7b440ecd
joaovitorsb97 <jvs.bastos@gmail.com>
2022-07-18 14:51:17 -0300

    Seller form

---
+++ src/gui/SellerFormController.java
@@ -0,0 +1,128 @@
+package gui;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+import db.DbException;
+import gui.listeners.DataChangeListener;
+import gui.util.Alerts;
+import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import model.entities.Seller;
+import model.exceptions.ValidationException;
+import model.services.SellerService;
+
+public class SellerFormController implements Initializable {
+
+	private Seller department;
+
+	private SellerService service;
+
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
+
+	@FXML
+	private TextField txtId;
+	@FXML
+	private TextField txtName;
+	@FXML
+	private Label labelError;
+	@FXML
+	private Button btnSave;
+	@FXML
+	private Button btnCancel;
+
+	@FXML
+	public void onBtnSaveAction(ActionEvent event) {
+		if (department == null || service == null) {
+			throw new IllegalStateException("Seller or Service was null!");
+		}
+		try {
+			department = getFormData();
+			service.saveOrUpdate(department);
+			notifyDataChangeListener();
+			Utils.currentStage(event).close();
+		} catch (DbException e) {
+			Alerts.showAlert("Error to save department data", null, e.getMessage(), AlertType.ERROR);
+		}catch(ValidationException e) {
+			setLabelError(e.getErrors());
+		}
+	}
+
+	private void notifyDataChangeListener() {
+		for (DataChangeListener listeners : dataChangeListeners) {
+			listeners.onDataChanged();
+		}
+
+	}
+
+	private Seller getFormData() {
+		Seller department = new Seller();
+
+		ValidationException exception = new ValidationException("Validation Error");
+
+		department.setId(Utils.tryParseToInt(txtId.getText()));
+
+		if (txtName.getText() == null || txtName.getText().trim().equals("")) {
+			exception.addErrors("name", "Field name can't be empty");
+		}
+		
+		department.setName(txtName.getText());
+		
+		if (exception.getErrors().size() > 0) {
+			throw exception;
+		}
+		
+		return department;
+	}
+
+	@FXML
+	public void onBtnCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
+	}
+
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+	}
+
+	public void initializeNodes() {
+		Constraints.setTextFieldInteger(txtId);
+		Constraints.setTextFieldMaxLength(txtName, 40);
+	}
+
+	public void setSeller(Seller department) {
+		this.department = department;
+	}
+
+	public void setSellerService(SellerService service) {
+		this.service = service;
+	}
+
+	public void subscribeDataChangeListener(DataChangeListener list) {
+		dataChangeListeners.add(list);
+	}
+
+	public void updateFormData() {
+		txtId.setText(String.valueOf(department.getId()));
+		txtName.setText(department.getName());
+	}
+	
+	public void setLabelError(Map<String, String> errors) {
+		Set<String> errorLabel = errors.keySet();
+		
+		if(errorLabel.contains("name")) {
+			labelError.setText(errors.get("name"));
+		}
+	}
+
+}
--- src/gui/SellerViewController.java
+++ src/gui/SellerViewController.java
@@ -1,5 +1,6 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.Date;
 import java.util.List;
@@ -16,7 +17,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
@@ -24,6 +27,8 @@ import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Seller;
 import model.services.SellerService;
@@ -106,29 +111,29 @@ public class SellerViewController implements Initializable, DataChangeListener {
 	}
 
 	private void createDialogForm(String absoluteName, Stage parentStage, Seller seller) {
-//		try {
-//			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-//			Pane pane = loader.load();
-//
-//			SellerFormController controller = loader.getController();
-//
-//			controller.setSeller(seller);
-//			controller.setSellerService(new SellerService());
-//			controller.subscribeDataChangeListener(this);
-//			controller.updateFormData(); // load data from object Seller
-//
-//			Stage dialogStage = new Stage();
-//
-//			dialogStage.setTitle("Enter seller data:");
-//			dialogStage.setScene(new Scene(pane));
-//			dialogStage.setResizable(false);
-//			dialogStage.initOwner(parentStage);
-//			dialogStage.initModality(Modality.WINDOW_MODAL);
-//			dialogStage.showAndWait();
-//
-//		} catch (IOException e) {
-//			Alerts.showAlert("Error IO Exception", null, e.getMessage(), AlertType.ERROR);
-//		}
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+
+			SellerFormController controller = loader.getController();
+
+			controller.setSeller(seller);
+			controller.setSellerService(new SellerService());
+			controller.subscribeDataChangeListener(this);
+			controller.updateFormData(); // load data from object Seller
+
+			Stage dialogStage = new Stage();
+
+			dialogStage.setTitle("Enter seller data:");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parentStage);
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+
+		} catch (IOException e) {
+			Alerts.showAlert("Error IO Exception", null, e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 	@Override
