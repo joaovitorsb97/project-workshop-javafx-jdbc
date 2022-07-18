7bdfdd1ecaa1e3ab40430998b0fcbd14b76b5577
joaovitorsb97 <jvs.bastos@gmail.com>
2022-07-18 18:11:51 -0300

    TextField & DatePicker

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -1,8 +1,11 @@
 package gui;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.Set;
@@ -17,6 +20,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Seller;
@@ -25,7 +29,7 @@ import model.services.SellerService;
 
 public class SellerFormController implements Initializable {
 
-	private Seller department;
+	private Seller seller;
 
 	private SellerService service;
 
@@ -36,7 +40,21 @@ public class SellerFormController implements Initializable {
 	@FXML
 	private TextField txtName;
 	@FXML
-	private Label labelError;
+	private TextField txtEmail;
+	@FXML
+	private DatePicker dpBirthDate;
+	@FXML
+	private TextField txtBaseSalary;
+	
+	@FXML
+	private Label labelErrorName;
+	@FXML
+	private Label labelErrorEmail;
+	@FXML
+	private Label labelErrorBirthDate;
+	@FXML
+	private Label labelErrorBaseSalary;
+	
 	@FXML
 	private Button btnSave;
 	@FXML
@@ -44,16 +62,16 @@ public class SellerFormController implements Initializable {
 
 	@FXML
 	public void onBtnSaveAction(ActionEvent event) {
-		if (department == null || service == null) {
+		if (seller == null || service == null) {
 			throw new IllegalStateException("Seller or Service was null!");
 		}
 		try {
-			department = getFormData();
-			service.saveOrUpdate(department);
+			seller = getFormData();
+			service.saveOrUpdate(seller);
 			notifyDataChangeListener();
 			Utils.currentStage(event).close();
 		} catch (DbException e) {
-			Alerts.showAlert("Error to save department data", null, e.getMessage(), AlertType.ERROR);
+			Alerts.showAlert("Error to save seller data", null, e.getMessage(), AlertType.ERROR);
 		}catch(ValidationException e) {
 			setLabelError(e.getErrors());
 		}
@@ -67,23 +85,23 @@ public class SellerFormController implements Initializable {
 	}
 
 	private Seller getFormData() {
-		Seller department = new Seller();
+		Seller seller = new Seller();
 
 		ValidationException exception = new ValidationException("Validation Error");
 
-		department.setId(Utils.tryParseToInt(txtId.getText()));
+		seller.setId(Utils.tryParseToInt(txtId.getText()));
 
 		if (txtName.getText() == null || txtName.getText().trim().equals("")) {
 			exception.addErrors("name", "Field name can't be empty");
 		}
 		
-		department.setName(txtName.getText());
+		seller.setName(txtName.getText());
 		
 		if (exception.getErrors().size() > 0) {
 			throw exception;
 		}
 		
-		return department;
+		return seller;
 	}
 
 	@FXML
@@ -98,10 +116,13 @@ public class SellerFormController implements Initializable {
 	public void initializeNodes() {
 		Constraints.setTextFieldInteger(txtId);
 		Constraints.setTextFieldMaxLength(txtName, 40);
+		Constraints.setTextFieldDouble(txtBaseSalary);
+		Constraints.setTextFieldMaxLength(txtEmail, 50);
+		Utils.formatDatePicker(dpBirthDate, "dd/MM/yyyy");
 	}
 
-	public void setSeller(Seller department) {
-		this.department = department;
+	public void setSeller(Seller seller) {
+		this.seller = seller;
 	}
 
 	public void setSellerService(SellerService service) {
@@ -113,15 +134,25 @@ public class SellerFormController implements Initializable {
 	}
 
 	public void updateFormData() {
-		txtId.setText(String.valueOf(department.getId()));
-		txtName.setText(department.getName());
+		Locale.setDefault(Locale.US);
+		if(seller == null) {
+			throw new IllegalStateException("Seller was null!");
+		}
+		txtId.setText(String.valueOf(seller.getId()));
+		txtName.setText(seller.getName());
+		txtEmail.setText(seller.getEmail());
+		txtBaseSalary.setText(String.format("%.2f", seller.getBaseSalary()));
+		if(seller.getBirthDate() != null) {
+			dpBirthDate.setValue(LocalDate.ofInstant(seller.getBirthDate().toInstant(), ZoneId.systemDefault()));
+
+		}
 	}
 	
 	public void setLabelError(Map<String, String> errors) {
 		Set<String> errorLabel = errors.keySet();
 		
 		if(errorLabel.contains("name")) {
-			labelError.setText(errors.get("name"));
+			labelErrorName.setText(errors.get("name"));
 		}
 	}
 
